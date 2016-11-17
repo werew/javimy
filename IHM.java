372bc3e9fdc24ad915bd893796aedfd5597e9762
Victor <victor.constans@etu.unistra.fr>
2016-11-17 23:59:35 +0100

    Add listener for open and quit button

--- IHM.java
+++ IHM.java
@@ -1,42 +1,74 @@
 import java.io.File;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class window extends JFrame implements ActionListener
+{
+		JMenuBar menu=new JMenuBar();
+		JMenu file=new JMenu("Fichier");
+		JButton btnOuvrir=new JButton("Ouvrir");
+		JButton btnEnregistrer=new JButton("Enregistrer");
+		JButton btnQuitter=new JButton("Quitter");
+
+	public window()
+	{
+		this.setTitle("Filtre");
+		this.setSize(500,500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.setJMenuBar(menu);
+
+		menu.add(file);
+
+		file.add(btnOuvrir);
+		btnOuvrir.addActionListener(this);
+
+		file.add(btnEnregistrer);
+
+		file.add(btnQuitter);
+		btnQuitter.addActionListener(this);
+
+		this.setVisible(true);
+
+	
+	}
+
+	private void ouvrir()
+	{
+		JFileChooser fichier=new JFileChooser(new File("."));
+		int boite=fichier.showOpenDialog(null);
+		if(boite==JFileChooser.APPROVE_OPTION)
+		{
+			System.out.println("Bravo, tu as choisi: "+fichier.getSelectedFile());	//XXX
+			
+		}
+		else
+		{
+			System.out.println("Perdu");	//XXX
+		}
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()==btnOuvrir)
+		{
+			this.ouvrir();
+		}
+		if(e.getSource()==btnQuitter)
+		{
+			this.dispose();
+		}
+	}
+	
+}
+
 
 public class IHM
 {
 	public static void main(String[] args)
 	{
-		JFrame window = new JFrame();
-		window.setTitle("Filtre");
-		window.setSize(500,500);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JMenuBar menu=new JMenuBar();
-		window.setJMenuBar(menu);
-
-		JMenu file=new JMenu("Fichier");
-		menu.add(file);
-
-		JButton btnOuvrir=new JButton("Ouvrir");
-		file.add(btnOuvrir);
-
-		JButton btnEnregistrer=new JButton("Enregistrer");
-		file.add(btnEnregistrer);
-
-		JButton btnQuitter=new JButton("Quitter");
-		file.add(btnQuitter);
-
-		window.setVisible(true);
-
-		JFileChooser fichier=new JFileChooser(new File("."));
-		int boite=fichier.showOpenDialog(null);
-		if(boite==JFileChooser.APPROVE_OPTION)
-		{
-			System.out.println("Bravo, tu as choisi: "+fichier.getSelectedFile());
-		}
-		else
-		{
-			System.out.println("Perdu");
-		}
-
+		window fenetre=new window();
 	}
 }
