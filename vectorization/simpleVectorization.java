d491ca5c0028ef53c2b6d3e9b8f7edd6e02cc44f
Victor <victor.constans@etu.unistra.fr>
2016-11-20 02:31:43 +0100

    Begin simple vectorization

---
+++ vectorization/simpleVectorization.java
@@ -0,0 +1,35 @@
+import filters.*;
+
+public class simpleVectorization
+{
+	private BufferedImage imageVect;
+	private Filter imageFiltrer;
+	private int seuil=200;
+
+	public simpleVectorization(BufferedImage image)
+	{
+		this.imageFiltrer=new Sobel(image);
+		int w=image.getWidth();
+		int h=image.getHeight();
+
+		imageVect=new BufferedImage(w,h,image.getType());
+		int i,j;
+
+		for(i=0;i<w;i++)
+		{
+			for(j=0;j<h;j++)
+			{
+				Color c=new Color(imageFiltrer.getRGB(i,j));
+				int red = (int)(c.getRed() * 0.3);
+				int green = (int)(c.getGreen() * 0.3);
+				int blue = (int)(c.getBlue() * 0.3);
+				int rgb = red+green+blue;
+
+				if(rgb>seuil)
+				{
+					
+				}
+			}
+		}
+	}
+}
