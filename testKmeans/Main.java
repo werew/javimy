0adc3775476e7ec1ad05914520f61c0b3add5688
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-11-23 15:13:44 +0100

    Remove compression

--- testKmeans/Main.java
+++ testKmeans/Main.java
@@ -1,6 +1,9 @@
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.*;
+import javax.imageio.*;
+import javax.imageio.stream.*;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String args[]){
@@ -9,12 +12,34 @@ public class Main {
         try {
             File input = new File("img.jpg");
             image = ImageIO.read(input);
-            Clusterizator f = new Clusterizator(image, 16); 
+            Clusterizator f = new Clusterizator(image, 2); 
             image = f.getImg();
             File output = new File("out.jpg");
-            ImageIO.write(image,"jpg",output); 
+            //ImageIO.write(image,"",output); 
+            writeJPG(image, new FileOutputStream(output), 1f);
+
+
         } catch (IOException e){
         };
 
     }
+
+public static void writeJPG(
+    BufferedImage bufferedImage,
+    OutputStream outputStream,
+    float quality) throws IOException
+{
+    Iterator<ImageWriter> iterator =
+        ImageIO.getImageWritersByFormatName("jpg");
+    ImageWriter imageWriter = iterator.next();
+    ImageWriteParam imageWriteParam = imageWriter.getDefaultWriteParam();
+    imageWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+    imageWriteParam.setCompressionQuality(quality);
+    ImageOutputStream imageOutputStream =
+        new MemoryCacheImageOutputStream(outputStream);
+    imageWriter.setOutput(imageOutputStream);
+    IIOImage iioimage = new IIOImage(bufferedImage, null, null);
+    imageWriter.write(null, iioimage, imageWriteParam);
+    imageOutputStream.flush();
+}
 }
